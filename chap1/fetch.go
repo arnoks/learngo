cab2526f3c29768256d3f824bd8293040ee6a8b0
Arno Schamber <arno@fam-schamber.de>
2018-06-24 22:05:43 +0200

    Adding excersise to fetch

--- chap1/fetch.go
+++ chap1/fetch.go
@@ -2,6 +2,19 @@
 // fetch <url> > content
 package main
 
+/*  Exercises:
+1.7 The funtion call io.Copy(dst,src) reads from src and writes to dst.
+Use it instead of ioutil.ReadAll to Copy the response body to os.Stdout
+without requiring a buffer large enough to hold the entire stream. Be sure to
+check the error result of io.Copy
+
+1.8 Modify fetch to add the prefix http:// to each argument URL if it is
+missing. You might want to use strings.HasPrefix
+
+1.9 Modify fetch to also print the HTTP status code, found in resp.Status
+
+*/
+
 import (
 	"fmt"
 	"io/ioutil"
