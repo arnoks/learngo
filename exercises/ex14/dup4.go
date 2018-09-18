9bf8a913a85bcb3ccc3526f6ba7e0ae93ffe91d3
Arno Schamber <arno.schamber@deutsche-boerse.com>
2018-09-18 08:55:54 +0200

    Adding exercise 1.4

--- exercises/ex14/dup4.go
+++ exercises/ex14/dup4.go
@@ -7,33 +7,51 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+type counter struct {
+	n     int
+	files map[string]bool
+}
 
 func main() {
 	files := os.Args[1:]
-	counts := make(map[string]int)
+	counts := make(map[string]*counter)
 
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, "stdin")
 	} else { // loop over the files
 		for _, file := range files {
 			fp, err := os.Open(file)
 			if err == nil {
 				countLines(fp, counts, file)
 			} else {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				fmt.Fprintf(os.Stderr, "dup4: %v\n", err)
 				continue
 			}
 		}
 	}
-	for line, n := range counts {
-		fmt.Printf("%d:\t%s\n", n, line)
+	for line, c := range counts {
+		fmt.Printf("%d:[%s]", c.n, line)
+		for f, _ := range c.files {
+			fmt.Printf("%s ", f)
+		}
+		fmt.Println()
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]*counter, filename string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		var t string = strings.TrimSpace(input.Text())
+		c, ok := counts[t]
+		if !ok {
+			c = new(counter)
+			c.files = make(map[string]bool)
+		}
+		c.n++
+		c.files[filename] = true
+		counts[t] = c
 	}
 }
