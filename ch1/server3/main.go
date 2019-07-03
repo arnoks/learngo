690ce1a7a28282da4f0f6479d0b1463e40eec77d
Arno Schamber <arno@fam-schamber.de>
2019-07-03 08:23:13 +0200

    reverting back to original server3

--- ch1/server3/main.go
+++ ch1/server3/main.go
@@ -1,31 +1,18 @@
-/* Server1 is a simple webserver which answers any url with the url string
- */
+// Server3 is a simple webserver which echoes the HTTP Request
 package main
 
 import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
-
-	"github.com/arnoks/learngo/chap1/lissajous"
 )
 
-var mu sync.Mutex
-var count int
-
 func main() {
-	http.HandleFunc("/", reqDetails)
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		lissajous.Lissajous(w)
-	}
-
-	http.HandleFunc("/lissajous", handler)
-	http.HandleFunc("/count", counter)
+	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
-func reqDetails(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
 	for k, v := range r.Header {
 		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
@@ -38,18 +25,4 @@ func reqDetails(w http.ResponseWriter, r *http.Request) {
 	for k, v := range r.Form {
 		fmt.Fprintf(w, "Form[%q] = %q", k, v)
 	}
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count += 1
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-}
-
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	count += 1
-	mu.Unlock()
 }
