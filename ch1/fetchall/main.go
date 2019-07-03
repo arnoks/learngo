db4c56017c773da312ed2b146a8e10c2bae30a82
Arno Schamber <arno@fam-schamber.de>
2019-07-03 08:03:10 +0200

    Adding some comments

--- ch1/fetchall/main.go
+++ ch1/fetchall/main.go
@@ -1,3 +1,4 @@
+//fetchall fetches all given urls reporting sizes and times
 package main
 
 import (
@@ -12,13 +13,15 @@ import (
 func main() {
 	start := time.Now()
 	ch := make(chan string)
+	// create go routine for each cmd argument and pass in both the url and the channel
 	for _, url := range os.Args[1:] {
 		go fetch(url, ch)
 	}
-
+	// read the reports from the channel and print to Stdout
 	for range os.Args[1:] {
 		fmt.Println(<-ch)
 	}
+	// print total time
 	fmt.Printf("%.2f elapsed\n", time.Since(start).Seconds())
 }
 
@@ -26,15 +29,15 @@ func fetch(url string, ch chan<- string) {
 	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
-		ch <- fmt.Sprint(err)
+		ch <- fmt.Sprint(err) // report get error
 		return
 	}
 	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
 	resp.Body.Close()
 	if err != nil {
-		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		ch <- fmt.Sprintf("while reading %s: %v", url, err) // report read error
 		return
 	}
 	secs := time.Since(start).Seconds()
-	ch <- fmt.Sprintf("%2fs %7d %s", secs, nbytes, url)
+	ch <- fmt.Sprintf("%2fs %7d %s", secs, nbytes, url) // report size and time
 }
