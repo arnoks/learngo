135fca5aa0cf77d5543a28b56b85c6f8f19322a4
Arno Schamber <arno.schamber@deutsche-boerse.com>
2019-07-08 08:59:52 +0200

    Adding exercise 2.1

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -1,11 +1,19 @@
 package tempconv
 
-import "fmt"
+// CtoF converts from Celsius to Fahrenheit
+func CtoF(c Celsius) Fahrenheit { return Fahrenheit(c*9.0/5.0 + 32.0) }
 
-// CToF converts from Celsius to Fahrenheit
-func CToF(c Celsius) Fahrenheit  { return Fahrenheit(c*9.0/5.0 + 32.0) }
-func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
+// CtoK converts from Celsius to Kelvin
+func CtoK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
 
-// FToC converts from Fahrenheit to  Celsius
-func FToC(f Fahrenheit) Celsius     { return Celsius((f - 32.0) * 5.0 / 9.0) }
-func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+// FtoC converts from Fahrenheit to  Celsius
+func FtoC(f Fahrenheit) Celsius { return Celsius((f - 32.0) * 5.0 / 9.0) }
+
+// FtoK converts from Fahrenheit to  Celsius
+func FtoK(f Fahrenheit) Kelvin { return Kelvin(((f - 32.0) * 5.0 / 9.0) + 273.15) }
+
+// KtoC converts from Celsius to Kelvin
+func KtoC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// KtoF converts from Kelvin to Fahrenheit
+func KtoF(k Kelvin) Fahrenheit { return Fahrenheit((k-273.15)*9.0/5.0 + 32.0) }
---
+++ exercises/ch2/ex2.1/main.go
@@ -0,0 +1,25 @@
+// ex2.1 is using tempconv to convert a given parameter into 3 different scales
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/arnoks/learngo/ch2/tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		} else {
+			f := tempconv.Fahrenheit(t)
+			c := tempconv.Celsius(t)
+			k := tempconv.Kelvin(t)
+			fmt.Printf("%v == %v\n%v == %v\n%v == %v\n", f, tempconv.FtoC(f), c, tempconv.CtoF(c), k, tempconv.KtoC(k))
+		}
+	}
+}
