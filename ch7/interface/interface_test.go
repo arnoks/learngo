bfd62b49226692d468604f4c9003d3cc6e2ec0a4
Arno Schamber <arno.schamber@deutsche-boerse.com>
2019-07-29 16:36:50 +0200

    Adding nil interface case

--- ch7/interface/interface_test.go
+++ ch7/interface/interface_test.go
@@ -24,7 +24,19 @@ func TestIf1(t *testing.T) {
 	fmt.Printf("nil: %v\n", w)
 }
 
-func TestPanic(t *testing.T) {
+func TestNilPointerWrite(t *testing.T) {
 	var w io.Writer
-	w.Write([]byte("hello"))
+	var bb bytes.Buffer
+	w = bytes.NewBuffer(make([]byte, 100))
+	n, err := w.Write([]byte("hello"))
+	if n > 0 && err == nil {
+		fmt.Printf("%v bytes written to buffer error:%v", n, err)
+	}
+	w = &bb
+	if w != nil {
+		n, err = w.Write([]byte("hello"))
+		if n > 0 && err == nil {
+			fmt.Printf("%v bytes written to buffer error:%v", n, err)
+		}
+	}
 }
